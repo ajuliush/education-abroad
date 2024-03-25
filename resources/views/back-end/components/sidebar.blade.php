f14439aa7a2b360349e68fcbaa8bf608fa37762e
ajuliush <ajuliush@gmail.com>
2024-03-25 16:16:25 +0600

    redesign

--- resources/views/back-end/components/sidebar.blade.php
+++ resources/views/back-end/components/sidebar.blade.php
@@ -1,6 +1,6 @@
   <div class="sidebar pe-4 pb-3">
       <nav class="navbar bg-secondary navbar-dark">
-          <a href="{{route('dashboard')}}" class="navbar-brand mx-4 mb-3">
+          <a href="{{route('dashboard.index')}}" class="navbar-brand mx-4 mb-3">
               <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>DarkPan</h3>
           </a>
           <div class="d-flex align-items-center ms-4 mb-4">
@@ -14,11 +14,11 @@
               </div>
           </div>
           <div class="navbar-nav w-100">
-              <a href="{{route('dashboard')}}" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+              <a href="{{route('dashboard.index')}}" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
               <div class="nav-item dropdown">
                   <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Elements</a>
                   <div class="dropdown-menu bg-transparent border-0">
-                      <a href="button.html" class="dropdown-item">Buttons</a>
+                      <a href="button.html" class="dropdown-item">Sample Table</a>
                       <a href="typography.html" class="dropdown-item">Typography</a>
                       <a href="element.html" class="dropdown-item">Other Elements</a>
                   </div>
